e76a1d0b8a9a8eade0f868abe4c65713ea91cdaa
George Hickman <george@incuna.com>
2011-10-04 16:44:29 +0100

    Add an inclusion tag to get total number of video hours
    Loop the vidoes adding each length to a timedelta and return a time to
    the template. Use a filter to get the hour value.

--- videos/templatetags/videotags.py
+++ videos/templatetags/videotags.py
@@ -1,4 +1,8 @@
+from datetime import datetime, timedelta, time
+
 from django import template
+
+from models import Video
 
 register = template.Library()
 @register.inclusion_tag('videos/_video.html')
@@ -6,6 +10,12 @@ def video(video):
     """
     Render the video.
     """
-    
     return {'video': video,}
+
+@register.inclusion_tag('videos/_video_hours_count.html')
+def video_hours_count():
+    total_time = timedelta()
+    for video in Video.objects.all():
+        total_time = total_time + timedelta(hours=video.length.hour, minutes=video.length.minute, seconds=video.length.second)
+    return {'hours': (datetime.min + total_time).time()}
 
