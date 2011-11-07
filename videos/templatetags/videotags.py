42806149c2b9c9dcfd29bacd7ca99abab7785b2a
George Hickman <george@ghickman.co.uk>
2011-11-07 11:56:27 +0000

    Round the video hours up if above 30 minutes and use a decimal instead of a time in the template
    The tag name suggests a number not a time.

--- videos/templatetags/videotags.py
+++ videos/templatetags/videotags.py
@@ -1,8 +1,8 @@
-from datetime import datetime, timedelta, time
+from datetime import datetime, timedelta
 
 from django import template
 
-from models import Video
+from videos.models import Video
 
 register = template.Library()
 @register.inclusion_tag('videos/_video.html')
@@ -17,5 +17,8 @@ def video_hours_count():
     total_time = timedelta()
     for video in Video.objects.all():
         total_time = total_time + timedelta(hours=video.length.hour, minutes=video.length.minute, seconds=video.length.second)
-    return {'hours': (datetime.min + total_time).time()}
+    result = (datetime.min + total_time)
+    if result.minute > 30:
+        result = result + timedelta(hours=1)
+    return {'hours': result.hour}
 
