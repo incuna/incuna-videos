3834e1b794044cb9df23c77bc9b8923ae8d2dbe9
Charlie Denton <charlie@meshy.co.uk>
2014-05-02 15:40:29 +0100

    Remove more django-incuna

--- videos/module/speakers/models.py
+++ videos/module/speakers/models.py
@@ -1,15 +1,22 @@
 from django.db import models
-from incuna.db.models import AutoSlugField 
+
 
 class Speaker(models.Model):
     """A person who features in a video"""
     name = models.CharField(max_length=127)
-    slug = AutoSlugField(max_length=127,populate_from="name",
-                         help_text='This will be automatically generated from the name, and is used as the speaker\'s website address', editable=True, blank=True, unique=True)
+    slug = models.SlugField(
+        max_length=127,
+        populate_from="name",
+        help_text=(
+            'This will be automatically generated from the name, and is used' +
+            'in the speaker\'s website address',
+        ),
+        editable=True,
+        unique=True,
+    )
 
     class Meta:
         app_label = 'videos'
 
     def __unicode__(self):
         return self.name
-
