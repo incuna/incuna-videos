b8111f908e0dc1f316293573a9f9f0de1746fa7f
Charlie Denton <charlie@meshy.co.uk>
2014-05-02 10:39:17 +0100

    Tidy setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name = "incuna-videos",
-    packages = find_packages(),
+    name='incuna-videos',
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        "FeinCMS>=1.7",
-        "django-settingsjs>=0.1",
+        'FeinCMS>=1.7',
+        'django-settingsjs>=0.1',
     ],
-    version = '1.0.0',
-    description = "Generic extensible video content.",
-    author = "Incuna Ltd",
-    author_email = "admin@incuna.com",
-    url = "http://incuna.com/",
+    version='1.0.0',
+    description='Generic extensible video content.',
+    author='Incuna Ltd',
+    author_email ='admin@incuna.com',
+    url='https://github.com/incuna/incuna-videos',
 )
