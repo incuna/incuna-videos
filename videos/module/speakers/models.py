15c4d5b9d501d082c9045d4b5500beebd1084786
Charlie Denton <charlie@meshy.co.uk>
2014-05-15 16:40:56 +0100

    Test Speaker model

--- videos/module/speakers/models.py
+++ videos/module/speakers/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Speaker(models.Model):
     """A person who features in a video"""
     name = models.CharField(max_length=127)
@@ -17,5 +19,5 @@ class Speaker(models.Model):
     class Meta:
         app_label = 'videos'
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
